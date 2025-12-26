baea7021e4f29b3d484dcced721f0a1604420126
Alex Sokolov <debugkeklov@yandex.ru>
2025-12-26 20:38:48 +0300

    Additions

--- winPizzaHub/Form1.Designer.cs
+++ winPizzaHub/Form1.Designer.cs
@@ -33,8 +33,8 @@ namespace winPizzaHub
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -80,20 +80,10 @@ namespace winPizzaHub
             this.groupBox1.Location = new System.Drawing.Point(50, 220);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(700, 200);
+            this.groupBox1.Size = new System.Drawing.Size(683, 200);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Пепперони";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.label3.Location = new System.Drawing.Point(483, 25);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(214, 22);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Иванов Иван Иванович";
             // 
             // label4
             // 
@@ -102,10 +92,20 @@ namespace winPizzaHub
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.Location = new System.Drawing.Point(182, 71);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(495, 117);
+            this.label4.Size = new System.Drawing.Size(478, 117);
             this.label4.TabIndex = 2;
             this.label4.Text = "Острая разновидность салями итало-американского происхождения. Обычно делается из" +
     " свинины.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label3.Location = new System.Drawing.Point(466, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Иванов Иван Иванович";
             // 
             // groupBox2
             // 
@@ -118,10 +118,10 @@ namespace winPizzaHub
             this.groupBox2.Location = new System.Drawing.Point(50, 439);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(700, 200);
+            this.groupBox2.Size = new System.Drawing.Size(683, 200);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "НАЗВАНИЕ ПИЦЦЫ";
+            this.groupBox2.Text = "Sardiniya";
             // 
             // label5
             // 
@@ -130,23 +130,23 @@ namespace winPizzaHub
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.Location = new System.Drawing.Point(182, 71);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(495, 117);
+            this.label5.Size = new System.Drawing.Size(478, 117);
             this.label5.TabIndex = 2;
-            this.label5.Text = "Текст. Описание пиццы соглано Вашему Варианту. Два, три предложения";
+            this.label5.Text = "Just testing";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Dock = System.Windows.Forms.DockStyle.Right;
-            this.label6.Location = new System.Drawing.Point(563, 25);
+            this.label6.Location = new System.Drawing.Point(561, 25);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(134, 22);
+            this.label6.Size = new System.Drawing.Size(119, 22);
             this.label6.TabIndex = 1;
-            this.label6.Text = "ФИО студента";
+            this.label6.Text = "Sokolov Alex";
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Image = global::winPizzaHub.Properties.Resources.template;
+            this.pictureBox3.Image = global::winPizzaHub.Properties.Resources.pizza19;
             this.pictureBox3.Location = new System.Drawing.Point(6, 28);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(160, 160);
@@ -180,7 +180,7 @@ namespace winPizzaHub
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(801, 561);
+            this.ClientSize = new System.Drawing.Size(818, 561);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
--- winPizzaHub/Properties/Resources.Designer.cs
+++ winPizzaHub/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace winPizzaHub.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -76,6 +76,16 @@ namespace winPizzaHub.Properties {
         internal static System.Drawing.Bitmap pizza00 {
             get {
                 object obj = ResourceManager.GetObject("pizza00", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pizza19 {
+            get {
+                object obj = ResourceManager.GetObject("pizza19", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
